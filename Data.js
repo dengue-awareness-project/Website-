bd1ebbad15de620df17879a55707fa563525d5c9
dengue-awareness-project <jadhavabhay499@gmail.com>
2025-10-05 22:56:41 +0530

    Create Data.js

---
+++ Data.js
@@ -0,0 +1,795 @@
+const dengueDataAllStates = {
+  "Andhra Pradesh": {
+    "2021": {
+      "cases": 6223,
+      "deaths": 13
+    },
+    "2022": {
+      "cases": 8540,
+      "deaths": 25
+    },
+    "2023": {
+      "cases": 10457,
+      "deaths": 51
+    },
+    "2024": {
+      "cases": 9362,
+      "deaths": 46
+    },
+    "2025": {
+      "cases": 10059,
+      "deaths": 23
+    }
+  },
+  "Arunachal Pradesh": {
+    "2021": {
+      "cases": 1592,
+      "deaths": 3
+    },
+    "2022": {
+      "cases": 2228,
+      "deaths": 6
+    },
+    "2023": {
+      "cases": 3290,
+      "deaths": 6
+    },
+    "2024": {
+      "cases": 1986,
+      "deaths": 9
+    },
+    "2025": {
+      "cases": 2835,
+      "deaths": 11
+    }
+  },
+  "Assam": {
+    "2021": {
+      "cases": 4282,
+      "deaths": 18
+    },
+    "2022": {
+      "cases": 5981,
+      "deaths": 32
+    },
+    "2023": {
+      "cases": 7320,
+      "deaths": 38
+    },
+    "2024": {
+      "cases": 5273,
+      "deaths": 19
+    },
+    "2025": {
+      "cases": 5133,
+      "deaths": 13
+    }
+  },
+  "Bihar": {
+    "2021": {
+      "cases": 14158,
+      "deaths": 33
+    },
+    "2022": {
+      "cases": 17918,
+      "deaths": 61
+    },
+    "2023": {
+      "cases": 25652,
+      "deaths": 113
+    },
+    "2024": {
+      "cases": 21835,
+      "deaths": 47
+    },
+    "2025": {
+      "cases": 21841,
+      "deaths": 90
+    }
+  },
+  "Chhattisgarh": {
+    "2021": {
+      "cases": 4592,
+      "deaths": 19
+    },
+    "2022": {
+      "cases": 6971,
+      "deaths": 30
+    },
+    "2023": {
+      "cases": 7604,
+      "deaths": 16
+    },
+    "2024": {
+      "cases": 5813,
+      "deaths": 29
+    },
+    "2025": {
+      "cases": 6120,
+      "deaths": 33
+    }
+  },
+  "Goa": {
+    "2021": {
+      "cases": 833,
+      "deaths": 2
+    },
+    "2022": {
+      "cases": 1147,
+      "deaths": 6
+    },
+    "2023": {
+      "cases": 1179,
+      "deaths": 4
+    },
+    "2024": {
+      "cases": 838,
+      "deaths": 3
+    },
+    "2025": {
+      "cases": 1226,
+      "deaths": 7
+    }
+  },
+  "Gujarat": {
+    "2021": {
+      "cases": 8639,
+      "deaths": 35
+    },
+    "2022": {
+      "cases": 11310,
+      "deaths": 30
+    },
+    "2023": {
+      "cases": 15515,
+      "deaths": 47
+    },
+    "2024": {
+      "cases": 10071,
+      "deaths": 47
+    },
+    "2025": {
+      "cases": 10280,
+      "deaths": 29
+    }
+  },
+  "Haryana": {
+    "2021": {
+      "cases": 2929,
+      "deaths": 10
+    },
+    "2022": {
+      "cases": 3071,
+      "deaths": 8
+    },
+    "2023": {
+      "cases": 5075,
+      "deaths": 27
+    },
+    "2024": {
+      "cases": 3254,
+      "deaths": 9
+    },
+    "2025": {
+      "cases": 4121,
+      "deaths": 14
+    }
+  },
+  "Himachal Pradesh": {
+    "2021": {
+      "cases": 1812,
+      "deaths": 5
+    },
+    "2022": {
+      "cases": 2393,
+      "deaths": 11
+    },
+    "2023": {
+      "cases": 3406,
+      "deaths": 10
+    },
+    "2024": {
+      "cases": 2502,
+      "deaths": 9
+    },
+    "2025": {
+      "cases": 2857,
+      "deaths": 10
+    }
+  },
+  "Jharkhand": {
+    "2021": {
+      "cases": 3640,
+      "deaths": 14
+    },
+    "2022": {
+      "cases": 4502,
+      "deaths": 22
+    },
+    "2023": {
+      "cases": 7518,
+      "deaths": 18
+    },
+    "2024": {
+      "cases": 5122,
+      "deaths": 13
+    },
+    "2025": {
+      "cases": 5949,
+      "deaths": 21
+    }
+  },
+  "Karnataka": {
+    "2021": {
+      "cases": 8632,
+      "deaths": 30
+    },
+    "2022": {
+      "cases": 10990,
+      "deaths": 45
+    },
+    "2023": {
+      "cases": 16074,
+      "deaths": 67
+    },
+    "2024": {
+      "cases": 11925,
+      "deaths": 24
+    },
+    "2025": {
+      "cases": 12529,
+      "deaths": 30
+    }
+  },
+  "Kerala": {
+    "2021": {
+      "cases": 18016,
+      "deaths": 82
+    },
+    "2022": {
+      "cases": 15982,
+      "deaths": 50
+    },
+    "2023": {
+      "cases": 21387,
+      "deaths": 81
+    },
+    "2024": {
+      "cases": 19310,
+      "deaths": 106
+    },
+    "2025": {
+      "cases": 18063,
+      "deaths": 106
+    }
+  },
+  "Madhya Pradesh": {
+    "2021": {
+      "cases": 13856,
+      "deaths": 78
+    },
+    "2022": {
+      "cases": 17591,
+      "deaths": 56
+    },
+    "2023": {
+      "cases": 21859,
+      "deaths": 88
+    },
+    "2024": {
+      "cases": 12713,
+      "deaths": 33
+    },
+    "2025": {
+      "cases": 14485,
+      "deaths": 60
+    }
+  },
+  "Maharashtra": {
+    "2021": {
+      "cases": 8066,
+      "deaths": 26
+    },
+    "2022": {
+      "cases": 7295,
+      "deaths": 17
+    },
+    "2023": {
+      "cases": 11108,
+      "deaths": 61
+    },
+    "2024": {
+      "cases": 7674,
+      "deaths": 22
+    },
+    "2025": {
+      "cases": 8603,
+      "deaths": 47
+    }
+  },
+  "Manipur": {
+    "2021": {
+      "cases": 618,
+      "deaths": 3
+    },
+    "2022": {
+      "cases": 775,
+      "deaths": 4
+    },
+    "2023": {
+      "cases": 1258,
+      "deaths": 6
+    },
+    "2024": {
+      "cases": 723,
+      "deaths": 3
+    },
+    "2025": {
+      "cases": 1062,
+      "deaths": 2
+    }
+  },
+  "Meghalaya": {
+    "2021": {
+      "cases": 2728,
+      "deaths": 15
+    },
+    "2022": {
+      "cases": 2806,
+      "deaths": 13
+    },
+    "2023": {
+      "cases": 4010,
+      "deaths": 23
+    },
+    "2024": {
+      "cases": 2893,
+      "deaths": 10
+    },
+    "2025": {
+      "cases": 3587,
+      "deaths": 13
+    }
+  },
+  "Mizoram": {
+    "2021": {
+      "cases": 1290,
+      "deaths": 2
+    },
+    "2022": {
+      "cases": 1384,
+      "deaths": 6
+    },
+    "2023": {
+      "cases": 2053,
+      "deaths": 8
+    },
+    "2024": {
+      "cases": 1219,
+      "deaths": 2
+    },
+    "2025": {
+      "cases": 1499,
+      "deaths": 4
+    }
+  },
+  "Nagaland": {
+    "2021": {
+      "cases": 660,
+      "deaths": 2
+    },
+    "2022": {
+      "cases": 696,
+      "deaths": 2
+    },
+    "2023": {
+      "cases": 925,
+      "deaths": 3
+    },
+    "2024": {
+      "cases": 641,
+      "deaths": 1
+    },
+    "2025": {
+      "cases": 836,
+      "deaths": 4
+    }
+  },
+  "Odisha": {
+    "2021": {
+      "cases": 7340,
+      "deaths": 21
+    },
+    "2022": {
+      "cases": 8885,
+      "deaths": 46
+    },
+    "2023": {
+      "cases": 10043,
+      "deaths": 23
+    },
+    "2024": {
+      "cases": 8875,
+      "deaths": 33
+    },
+    "2025": {
+      "cases": 9068,
+      "deaths": 33
+    }
+  },
+  "Punjab": {
+    "2021": {
+      "cases": 6501,
+      "deaths": 30
+    },
+    "2022": {
+      "cases": 10721,
+      "deaths": 25
+    },
+    "2023": {
+      "cases": 12415,
+      "deaths": 60
+    },
+    "2024": {
+      "cases": 10457,
+      "deaths": 56
+    },
+    "2025": {
+      "cases": 9541,
+      "deaths": 26
+    }
+  },
+  "Rajasthan": {
+    "2021": {
+      "cases": 7012,
+      "deaths": 17
+    },
+    "2022": {
+      "cases": 7405,
+      "deaths": 23
+    },
+    "2023": {
+      "cases": 9895,
+      "deaths": 54
+    },
+    "2024": {
+      "cases": 6223,
+      "deaths": 23
+    },
+    "2025": {
+      "cases": 9277,
+      "deaths": 22
+    }
+  },
+  "Sikkim": {
+    "2021": {
+      "cases": 1930,
+      "deaths": 11
+    },
+    "2022": {
+      "cases": 2927,
+      "deaths": 7
+    },
+    "2023": {
+      "cases": 4251,
+      "deaths": 16
+    },
+    "2024": {
+      "cases": 2993,
+      "deaths": 16
+    },
+    "2025": {
+      "cases": 2916,
+      "deaths": 10
+    }
+  },
+  "Tamil Nadu": {
+    "2021": {
+      "cases": 18900,
+      "deaths": 107
+    },
+    "2022": {
+      "cases": 26168,
+      "deaths": 82
+    },
+    "2023": {
+      "cases": 32925,
+      "deaths": 86
+    },
+    "2024": {
+      "cases": 21496,
+      "deaths": 61
+    },
+    "2025": {
+      "cases": 20711,
+      "deaths": 89
+    }
+  },
+  "Telangana": {
+    "2021": {
+      "cases": 5457,
+      "deaths": 27
+    },
+    "2022": {
+      "cases": 6931,
+      "deaths": 15
+    },
+    "2023": {
+      "cases": 10881,
+      "deaths": 43
+    },
+    "2024": {
+      "cases": 8128,
+      "deaths": 21
+    },
+    "2025": {
+      "cases": 9149,
+      "deaths": 21
+    }
+  },
+  "Tripura": {
+    "2021": {
+      "cases": 917,
+      "deaths": 4
+    },
+    "2022": {
+      "cases": 1480,
+      "deaths": 8
+    },
+    "2023": {
+      "cases": 1719,
+      "deaths": 4
+    },
+    "2024": {
+      "cases": 1464,
+      "deaths": 6
+    },
+    "2025": {
+      "cases": 1494,
+      "deaths": 6
+    }
+  },
+  "Uttar Pradesh": {
+    "2021": {
+      "cases": 10914,
+      "deaths": 50
+    },
+    "2022": {
+      "cases": 12208,
+      "deaths": 70
+    },
+    "2023": {
+      "cases": 14496,
+      "deaths": 67
+    },
+    "2024": {
+      "cases": 11122,
+      "deaths": 32
+    },
+    "2025": {
+      "cases": 13393,
+      "deaths": 33
+    }
+  },
+  "Uttarakhand": {
+    "2021": {
+      "cases": 1678,
+      "deaths": 5
+    },
+    "2022": {
+      "cases": 1629,
+      "deaths": 3
+    },
+    "2023": {
+      "cases": 2710,
+      "deaths": 16
+    },
+    "2024": {
+      "cases": 1485,
+      "deaths": 3
+    },
+    "2025": {
+      "cases": 1860,
+      "deaths": 7
+    }
+  },
+  "West Bengal": {
+    "2021": {
+      "cases": 11497,
+      "deaths": 63
+    },
+    "2022": {
+      "cases": 13107,
+      "deaths": 45
+    },
+    "2023": {
+      "cases": 16100,
+      "deaths": 60
+    },
+    "2024": {
+      "cases": 14322,
+      "deaths": 68
+    },
+    "2025": {
+      "cases": 16055,
+      "deaths": 32
+    }
+  },
+  "Andaman and Nicobar": {
+    "2021": {
+      "cases": 2552,
+      "deaths": 14
+    },
+    "2022": {
+      "cases": 2706,
+      "deaths": 15
+    },
+    "2023": {
+      "cases": 3600,
+      "deaths": 11
+    },
+    "2024": {
+      "cases": 2436,
+      "deaths": 12
+    },
+    "2025": {
+      "cases": 2950,
+      "deaths": 9
+    }
+  },
+  "Chandigarh": {
+    "2021": {
+      "cases": 2515,
+      "deaths": 12
+    },
+    "2022": {
+      "cases": 3095,
+      "deaths": 14
+    },
+    "2023": {
+      "cases": 4112,
+      "deaths": 16
+    },
+    "2024": {
+      "cases": 2896,
+      "deaths": 16
+    },
+    "2025": {
+      "cases": 2932,
+      "deaths": 6
+    }
+  },
+  "Dadra and Nagar Haveli and Daman and Diu": {
+    "2021": {
+      "cases": 2413,
+      "deaths": 7
+    },
+    "2022": {
+      "cases": 2014,
+      "deaths": 6
+    },
+    "2023": {
+      "cases": 2814,
+      "deaths": 12
+    },
+    "2024": {
+      "cases": 2056,
+      "deaths": 5
+    },
+    "2025": {
+      "cases": 2575,
+      "deaths": 10
+    }
+  },
+  "Lakshadweep": {
+    "2021": {
+      "cases": 2149,
+      "deaths": 4
+    },
+    "2022": {
+      "cases": 2103,
+      "deaths": 12
+    },
+    "2023": {
+      "cases": 3408,
+      "deaths": 19
+    },
+    "2024": {
+      "cases": 2359,
+      "deaths": 5
+    },
+    "2025": {
+      "cases": 2516,
+      "deaths": 10
+    }
+  },
+  "Delhi": {
+    "2021": {
+      "cases": 12516,
+      "deaths": 31
+    },
+    "2022": {
+      "cases": 14092,
+      "deaths": 48
+    },
+    "2023": {
+      "cases": 15748,
+      "deaths": 88
+    },
+    "2024": {
+      "cases": 12002,
+      "deaths": 56
+    },
+    "2025": {
+      "cases": 13051,
+      "deaths": 44
+    }
+  },
+  "Jammu and Kashmir": {
+    "2021": {
+      "cases": 2787,
+      "deaths": 16
+    },
+    "2022": {
+      "cases": 2830,
+      "deaths": 16
+    },
+    "2023": {
+      "cases": 3836,
+      "deaths": 20
+    },
+    "2024": {
+      "cases": 2597,
+      "deaths": 14
+    },
+    "2025": {
+      "cases": 2703,
+      "deaths": 7
+    }
+  },
+  "Ladakh": {
+    "2021": {
+      "cases": 1723,
+      "deaths": 8
+    },
+    "2022": {
+      "cases": 1928,
+      "deaths": 5
+    },
+    "2023": {
+      "cases": 2950,
+      "deaths": 7
+    },
+    "2024": {
+      "cases": 1527,
+      "deaths": 8
+    },
+    "2025": {
+      "cases": 1994,
+      "deaths": 5
+    }
+  },
+  "Puducherry": {
+    "2021": {
+      "cases": 2027,
+      "deaths": 6
+    },
+    "2022": {
+      "cases": 2960,
+      "deaths": 16
+    },
+    "2023": {
+      "cases": 3090,
+      "deaths": 6
+    },
+    "2024": {
+      "cases": 2105,
+      "deaths": 7
+    },
+    "2025": {
+      "cases": 2550,
+      "deaths": 13
+    }
+  }
+};
+const availableYears = [2021, 2022, 2023, 2024, 2025];
